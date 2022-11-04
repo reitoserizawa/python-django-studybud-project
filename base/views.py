c03988bfa3c0f76890f2d92c495d33dc7a6685bd
ReitoSerizawa <s.reitiger@gmail.com>
2022-11-04 12:14:30 -0400

    database and admin set

--- base/views.py
+++ base/views.py
@@ -1,18 +1,11 @@
 from django.shortcuts import render
-
-rooms = [
-    {'id': 1, 'name': "Let's learn python!"},
-    {'id': 2, 'name': "Design with me"},
-    {'id': 3, 'name': "Frontend developers"}
-]
+from .models import Room
 
 def home(request):
+    rooms = Room.objects.all()
     return render(request, 'base/home.html', {'rooms': rooms})
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
